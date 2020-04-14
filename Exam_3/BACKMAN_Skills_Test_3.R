9d7817721848deafc2fd4c2ec8dac9550609c51c
talia-backman <talia.backman@gmail.com>
2020-04-14 13:53:17 -0600

    exam 3

---
+++ Exam_3/BACKMAN_Skills_Test_3.R
@@ -0,0 +1,6 @@
+#Your tasks:
+#1. Do an analysis of the mtcars data set. That's right, our old friend mpg will be the dependent variable.
+#2. Do your analysis in an Rmarkdown document, complete with explanations for what is going on, what models you are trying, and figures showing predicted vs real data, etc.
+#3. Knit that Rmarkdown to an html file and add it to your website with a link that is easy for me to find on your page.
+
+data("mtcars")
